f31ba19f1e57669c46f296b8a57048eb0a33506a
Deepali <deepali1989@gmail.com>
2020-07-19 11:20:52 +0530

    Initial Commit

---
+++ iMunch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iMunch
+//
+//  Created by DEEPALI MAHESHWARI on 19/07/20.
+//  Copyright © 2020 DEEPALI MAHESHWARI. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iMunchTests/iMunchTests.swift
@@ -0,0 +1,34 @@
+//
+//  iMunchTests.swift
+//  iMunchTests
+//
+//  Created by DEEPALI MAHESHWARI on 19/07/20.
+//  Copyright © 2020 DEEPALI MAHESHWARI. All rights reserved.
+//
+
+import XCTest
+@testable import iMunch
+
+class iMunchTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
